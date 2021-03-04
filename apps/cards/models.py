4cf2c83bda31befe9f1697bc734c772032266e73
mkh <elric2009@gmail.com>
2021-03-04 11:27:14 +1000

    add decks and cards apps

---
+++ apps/cards/models.py
@@ -0,0 +1,8 @@
+# from django.utils import timezone
+# from django.db import models
+# from apps.decks.models import Deck
+# from apps.utils.models import Timestamps
+
+
+# class Card(Timestamps):
+#     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
