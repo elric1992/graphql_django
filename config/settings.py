a194f1195bbceef5e2389f55c88dd18c98db6283
mkh <elric2009@gmail.com>
2021-03-15 17:54:22 +1000

    49:57

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'apps.users'
+    'apps.users',
+    'apps.decks',
+    'apps.cards',
+
 ]
 
 MIDDLEWARE = [
